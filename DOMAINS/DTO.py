214327169ab1ff32b9ce01bc2b7e6cad3fd04e92
stormy_mm <maksutenkomihail@gmail.com>
2026-02-22 12:27:13 +0300

    удалены некоторые файлы

--- DOMAINS/DTO.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class TaskDTO:
-    """Класс для представления задачи"""
-    id: int
-    title: str
-    status : str
-    description: str
--- user_input.py
+++
@@ -1,7 +0,0 @@
-# class UserInput:
-#     """Класс для пользовательского ввода"""
-#
-#     @staticmethod
-#     def input_user():
-#         """Возвращает пользовательский ввод"""
-#         return input(">>> ")
