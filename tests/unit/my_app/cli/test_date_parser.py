3f7f7ba572846688d2c9d08cf2a4fda825710319
stormy_mm <maksutenkomihail@gmail.com>
2026-02-23 12:27:10 +0300

    добавлен тест на проверку парсинга даты

---
+++ tests/unit/my_app/cli/test_date_parser.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from my_app.cli.date_parser import ParsingDate
+
+def test_date_parser():
+    """Тест для проверки парсинга даты"""
+    assert ParsingDate(view_date=datetime(2020, 1, 1, tzinfo=ZoneInfo('UTC'))).date_format == '01.01.2020 00:00:00'
